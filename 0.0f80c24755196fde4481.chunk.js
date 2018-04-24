0c89fcea63ed96c24ba2e730e374241fb6e8dca5
Nazmul Alam <nazmuldipu@gmail.com>
2018-04-24 21:04:42 +0600

    Auto-generated commit

--- 0.0f80c24755196fde4481.chunk.js
+++
@@ -1,1 +0,0 @@
-webpackJsonp([0],{j0WS:function(t,i,s){"use strict";s.d(i,"a",function(){return n});var n=function(t,i,s,n,e,a,c,h){this.id=t,this.customerId=i,this.date=s,this.explanation=n,this.ref=e,this.debit=a,this.credit=c,this.balance=h}}});
